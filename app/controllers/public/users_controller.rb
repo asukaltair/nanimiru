503e2941e9d49358a5b83d5abec07894fd7b7e9f
vagrant <vagrant@localhost.localdomain>
2020-08-09 18:13:05 +0000

    create user

---
+++ app/controllers/public/users_controller.rb
@@ -0,0 +1,33 @@
+class Public::UsersController < ApplicationController
+
+	before_action :authenticate_user!
+	before_action :ensure_user, only: [:show, :edit, :update]
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+		if @user.update(user_params)
+			redirect_to user_path
+		end
+
+	end
+
+	private
+
+	def ensure_user
+		@user = User.find_by(name: params[:name])
+	end
+
+	def user_params
+		params.require(:user).permit(
+			:name,
+			:introduction,
+			:email,
+			:image)
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,13 @@ Rails.application.routes.draw do
 		passwords:     'public/passwords',
 		registrations: 'public/registrations'
 	}
+
+	scope module: :public do
+
+		get 'users/:name' => 'users#show', as: 'user'
+		get 'users/:name/edit' => 'users#edit', as: 'user_edit'
+		patch 'users/:name' => 'users#update'
+
+	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
