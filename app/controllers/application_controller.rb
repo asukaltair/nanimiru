e7bdd0f0fd11bcb22771350156587e500209e792
vagrant <vagrant@localhost.localdomain>
2020-08-09 18:05:58 +0000

    create devise user_and_admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	protected
+
+	def after_sign_up_path_for(resourse)
+		root_path
+	end
+
+	def after_sign_out_path_for(resourse)
+		root_path
+	end
+
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+		devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+
+	devise_for :admins, controllers: {
+		sessions:      'admins/sessions',
+		passwords:     'admins/passwords',
+		registrations: 'admins/registrations'
+	}
+
+	devise_for :users, controllers: {
+		sessions:      'public/sessions',
+		passwords:     'public/passwords',
+		registrations: 'public/registrations'
+	}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
