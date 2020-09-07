a650a672a7fbb2bc24135383f691bd577de9ffef
vagrant <vagrant@localhost.localdomain>
2020-09-07 08:59:30 +0000

    hide gmail-information

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,8 +40,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                  587,
     domain:               'gmail.com',
-    user_name:            'asuka.altair@gmail.com',
-    password:             'czakeyumtexcytny',
+    user_name:            ENV['GMAIL_USERNAME'],
+    password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto:  true
   }
